63cf65d654bbe93ea6dda4a3370a2a421f84990a
BenCook28 <benthomascook28@gmail.com>
2021-02-02 20:31:45 -0500

    fixed AppDbContext errors

--- BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/Models/AppDbContext.cs
@@ -8,9 +8,13 @@ namespace BethanysPieShop.Models
 {
     public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
 
+        }
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 
-    public DbSet<Pie> Pies { get; set; }
-    public DbSet<Category> Categories { get; set; }
+    
 }
